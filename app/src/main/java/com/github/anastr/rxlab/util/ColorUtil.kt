b5aaf16f1c6ca31051bcd1817c796e9d1fac4744
Anas Altair <anastr244@gmail.com>
2020-04-01 16:47:11 +0300

    add colors util.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/util/ColorUtil.kt
@@ -0,0 +1,30 @@
+package com.github.anastr.rxlab.util
+
+import kotlin.random.Random
+
+/**
+ * Created by Anas Altair on 3/27/2020.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
+class ColorUtil {
+    companion object {
+        const val red = 0xFFF44336.toInt()
+        const val pink = 0xFFFF4081.toInt()
+        const val purple = 0xFFAB47BC.toInt()
+        const val litePurple = 0xFFE040FB.toInt()
+        const val deepPurple = 0xFF7E57C2.toInt()
+        const val blue = 0xFF448AFF.toInt()
+        const val liteBlue = 0xFF00B0FF.toInt()
+        const val blueGray = 0xFF607D8B.toInt()
+        const val green = 0xFF43A047.toInt()
+        const val orange = 0xFFEF6C00.toInt()
+        const val deepOrange = 0xFFF4511E.toInt()
+        const val brown = 0xFF795548.toInt()
+        const val gray = 0xFF757575.toInt()
+
+        private val colors = arrayOf(red, pink, purple, litePurple, deepPurple, blue, liteBlue
+            , blueGray, green, orange, deepOrange, brown, gray)
+
+        fun randomColor(): Int = colors[Random.nextInt(colors.size)]
+    }
+}
