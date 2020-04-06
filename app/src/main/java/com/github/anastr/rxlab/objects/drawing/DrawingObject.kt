12c248f03a0bbde6cae5ecc75ce588ceda640a77
Anas Altair <anastr244@gmail.com>
2020-04-06 14:43:04 +0300

    update movement.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

--- app/src/main/java/com/github/anastr/rxlab/objects/drawing/DrawingObject.kt
+++ app/src/main/java/com/github/anastr/rxlab/objects/drawing/DrawingObject.kt
@@ -1,17 +1,13 @@
 package com.github.anastr.rxlab.objects.drawing
 
-import android.animation.ValueAnimator
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.text.TextPaint
-import android.view.animation.DecelerateInterpolator
-import androidx.core.animation.doOnEnd
 import com.github.anastr.rxlab.objects.EmitObject
 import com.github.anastr.rxlab.util.Point
 import com.github.anastr.rxlab.util.Utils
-import com.github.anastr.rxlab.util.doOnMainThread
 import com.github.anastr.rxlab.util.dpToPx
 import java.util.*
 import kotlin.collections.ArrayList
@@ -46,63 +42,25 @@ abstract class DrawingObject(val name: String) {
     protected open fun onAddEmits(emitObject: EmitObject) { }
 
     /**
-     *
-     * **must be called on main thread.**
-     */
-    fun moveEmit(emit: EmitObject, to: Point, onEnd: () -> Any = {}) {
-        val distanceX = to.x - emit.rect.left
-        val distanceY = to.y - emit.rect.top
-        var pre = 0f
-        ValueAnimator.ofFloat(0f, 1f).apply {
-            duration = 500
-            interpolator = DecelerateInterpolator()
-            addUpdateListener { animation ->
-                emit.rect.offset(distanceX*(animation.animatedFraction - pre)
-                    , distanceY*(animation.animatedFraction - pre))
-                pre = animation.animatedFraction
-            }
-            doOnEnd { onEnd.invoke() }
-        }.start()
-    }
-
-    /**
-     * add emit immediately to `insertPoint`.
-     *
-     * **must be called on render thread.**
-     */
-    fun addEmit(emit: EmitObject) {
-        addEmit(emit, getInsertPoint())
-        onEmitsChanged()
-    }
-
-    /**
-     * add emit without change its position, then move it
-     * to `insertPoint` with animation.
-     *
-     * **must be called on render thread.**
-     */
-    fun addEmitWithAnimation(emit: EmitObject) {
-        addEmit(emit, emit.position)
-        onEmitsChanged()
-        doOnMainThread {
-            moveEmit(emit, getInsertPoint())
-        }
-    }
-
-    /**
      * add list of emits immediately to `insertPoint`.
      *
      * **must be called on render thread.**
      */
     fun addEmits(emits: List<EmitObject>) {
         emits.forEach { addEmit(it, getInsertPoint()) }
-        onEmitsChanged()
     }
 
-    private fun addEmit(emit: EmitObject, initial: Point) {
+
+    /**
+     * add emit immediately to `insertPoint`, or other point.
+     *
+     * **must be called on render thread.**
+     */
+    fun addEmit(emit: EmitObject, initial: Point = getInsertPoint()) {
         emit.offsetTo(initial)
         emitObjects.add(emit)
         onAddEmits(emit)
+        onEmitsChanged()
     }
 
     /**
