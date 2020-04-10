4ec9f1954684e8c27407a19d0dc2525aaf9b60b4
Anas Altair <anastr244@gmail.com>
2020-04-10 10:40:00 +0300

    add fromIterable activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/activities/FromIterableActivity.kt
@@ -0,0 +1,42 @@
+package com.github.anastr.rxlab.activities
+
+import android.os.Bundle
+import com.github.anastr.rxlab.objects.BallEmit
+import com.github.anastr.rxlab.objects.drawing.FixedEmitsOperation
+import com.github.anastr.rxlab.objects.drawing.ObserverObject
+import com.github.anastr.rxlab.view.Action
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_operation.*
+
+/**
+ * Created by Anas Altair on 4/1/2020.
+ */
+class FromIterableActivity: OperationActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCode("List list = Arrays.asList(\"L\", \"i\", \"s\", \"t\");\n" +
+                "Observable.fromIterable(list)\n" +
+                "        .subscribe();")
+
+
+        val list = listOf(BallEmit("L"), BallEmit("i"), BallEmit("s"), BallEmit("t"))
+
+        val fromIterableOperation = FixedEmitsOperation("fromIterable", list)
+        surfaceView.addDrawingObject(fromIterableOperation)
+        val observerObject = ObserverObject("Observer")
+        surfaceView.addDrawingObject(observerObject)
+
+        val actions = ArrayList<Action>()
+
+        Observable.fromIterable(list)
+            .subscribe({
+                actions.add(Action(1000) { moveEmit(it, fromIterableOperation, observerObject) })
+            }, errorHandler, {
+                actions.add(Action(0) { doOnRenderThread { observerObject.complete() } })
+                surfaceView.actions(actions)
+            })
+            .disposeOnDestroy()
+    }
+
+}
