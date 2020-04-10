5ba3a4c6457ad8188bc4b4aa3f1b30c745710ae0
Anas Altair <anastr244@gmail.com>
2020-04-10 10:46:28 +0300

    add list activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/adapter/MyAdapter.kt
@@ -0,0 +1,42 @@
+package com.github.anastr.rxlab.adapter
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.anastr.rxlab.R
+
+/**
+ * Created by Anas Altair on 4/1/2020.
+ */
+class MyAdapter(val activity: Activity, val operations: List<OperationData>)
+    : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val v: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_item, parent, false)
+        return MyViewHolder(v)
+    }
+
+    override fun getItemCount() = operations.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.titleView.text = operations[position].name
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val titleView: TextView = itemView.findViewById(R.id.textView)
+
+        init {
+            itemView.setOnClickListener {
+                val intent = Intent(activity, operations[adapterPosition].clazz)
+                intent.putExtra("title", operations[adapterPosition].name)
+                activity.startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/github/anastr/rxlab/adapter/OperationData.kt
@@ -0,0 +1,8 @@
+package com.github.anastr.rxlab.adapter
+
+import android.app.Activity
+
+/**
+ * Created by Anas Altair on 4/1/2020.
+ */
+data class OperationData(val name: String, val clazz: Class<out Activity>)
