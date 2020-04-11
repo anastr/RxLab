a879d013883521e2edd128cc367d9e2a3b1bb2a9
Anas Altair <anastr244@gmail.com>
2020-04-11 14:03:08 +0300

    add tack activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/activities/TakeActivity.kt
@@ -0,0 +1,47 @@
+package com.github.anastr.rxlab.activities
+
+import android.os.Bundle
+import com.github.anastr.rxlab.objects.emits.BallEmit
+import com.github.anastr.rxlab.objects.drawing.ObserverObject
+import com.github.anastr.rxlab.objects.drawing.TextOperation
+import com.github.anastr.rxlab.view.Action
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_operation.*
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Anas Altair on 4/6/2020.
+ */
+class TakeActivity: OperationActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCode("Observable.interval(1000, TimeUnit.MILLISECONDS)\n" +
+                "        .take(3)\n" +
+                "        .subscribe();")
+
+        val intervalOperation = TextOperation("interval", "1000 milliseconds (take 3)")
+        surfaceView.addDrawingObject(intervalOperation)
+        val observerObject = ObserverObject("Observer")
+        surfaceView.addDrawingObject(observerObject)
+
+        val actions = ArrayList<Action>()
+
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+            .take(3)
+            .subscribe({
+                val thread = Thread.currentThread().name
+                actions.add(Action(1000) {
+                    val emit =
+                        BallEmit("${(it + 1)} sec")
+                    emit.checkThread(thread)
+                    addEmit(intervalOperation, emit)
+                    moveEmit(emit, intervalOperation, observerObject)
+                })
+            }, errorHandler, {
+                actions.add(Action(0) { doOnRenderThread { observerObject.complete() } })
+                surfaceView.actions(actions)
+            })
+            .disposeOnDestroy()
+    }
+}
