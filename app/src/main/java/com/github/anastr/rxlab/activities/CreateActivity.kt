67f897baa2e1a5f30321776ec48e6cde2ad8a766
Anas Altair <anastr244@gmail.com>
2020-04-12 19:16:03 +0300

    add create activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/activities/CreateActivity.kt
@@ -0,0 +1,48 @@
+package com.github.anastr.rxlab.activities
+
+import android.os.Bundle
+import android.view.View
+import com.github.anastr.rxlab.objects.emits.BallEmit
+import com.github.anastr.rxlab.objects.emits.EmitObject
+import com.github.anastr.rxlab.objects.drawing.ObserverObject
+import com.github.anastr.rxlab.view.Action
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_operation.*
+
+/**
+ * Created by Anas Altair on 4/10/2020.
+ */
+class CreateActivity: OperationActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCode("Observable.<String>create(emitter -> {\n" +
+                "    fab.setOnClickListener(v1 -> {\n" +
+                "        if (!emitter.isDisposed())\n" +
+                "            emitter.onNext(\"emit\");\n" +
+                "    });\n" +
+                "}).subscribe();")
+
+        fab.visibility = View.VISIBLE
+
+        val observerObject = ObserverObject("Observer")
+        surfaceView.addDrawingObject(observerObject)
+
+        Observable.create<EmitObject> { emitter ->
+            fab.setOnClickListener {
+                if (!emitter.isDisposed)
+                    emitter.onNext(
+                        BallEmit(
+                            "emit"
+                        )
+                    )
+            }
+        }
+            .subscribe({
+                surfaceView.action(Action(0) { addEmit(observerObject, it) })
+            }, errorHandler, {
+                surfaceView.action(Action(0) { doOnRenderThread { observerObject.complete() } })
+            })
+            .disposeOnDestroy()
+    }
+}
