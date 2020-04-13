bd47404c289548710770ec67732b0ac055b03226
Anas Altair <anastr244@gmail.com>
2020-04-13 19:13:49 +0300

    add takeLast activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/activities/TakeLastActivity.kt
@@ -0,0 +1,51 @@
+package com.github.anastr.rxlab.activities
+
+import android.os.Bundle
+import com.github.anastr.rxlab.objects.drawing.FixedEmitsOperation
+import com.github.anastr.rxlab.objects.drawing.ObserverObject
+import com.github.anastr.rxlab.objects.emits.BallEmit
+import com.github.anastr.rxlab.view.Action
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_operation.*
+
+/**
+ * Created by Anas Altair on 4/13/2020.
+ */
+class TakeLastActivity: OperationActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCode("Observable.just(1, 2, 3, 4, 5)\n" +
+                "        .takeLast(3)\n" +
+                "        .subscribe();")
+
+        addNote("be aware that 'takeLast(int)' method will collect all emits in queue " +
+                "and then will emit the last specified number of emits.")
+
+        val e1 = BallEmit("1")
+        val e2 = BallEmit("2")
+        val e3 = BallEmit("3")
+        val e4 = BallEmit("4")
+        val e5 = BallEmit("5")
+
+        val justOperation = FixedEmitsOperation("just", listOf(e1, e2, e3, e4, e5))
+        surfaceView.addDrawingObject(justOperation)
+        val takeLastOperation = FixedEmitsOperation("takeLast", ArrayList())
+        surfaceView.addDrawingObject(takeLastOperation)
+        val observerObject = ObserverObject("Observer")
+        surfaceView.addDrawingObject(observerObject)
+
+        val actions = ArrayList<Action>()
+
+        Observable.just(e1, e2, e3, e4, e5)
+            .doOnNext { actions.add(Action(1000) { moveEmit(it, justOperation, takeLastOperation) }) }
+            .takeLast(3)
+            .subscribe({
+                actions.add(Action(1000) { moveEmit(it, takeLastOperation, observerObject) })
+            }, errorHandler, {
+                actions.add(Action(0) { doOnRenderThread { observerObject.complete() } })
+                surfaceView.actions(actions)
+            })
+            .disposeOnDestroy()
+    }
+}
