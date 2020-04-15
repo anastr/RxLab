771e7ccc5740dc45e344e369bf46f470bc35047f
Anas Altair <anastr244@gmail.com>
2020-04-15 17:36:53 +0300

    add add time objects to interval activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

--- app/src/main/java/com/github/anastr/rxlab/activities/IntervalActivity.kt
+++ app/src/main/java/com/github/anastr/rxlab/activities/IntervalActivity.kt
@@ -1,9 +1,12 @@
 package com.github.anastr.rxlab.activities
 
 import android.os.Bundle
-import com.github.anastr.rxlab.objects.emits.BallEmit
+import android.os.Handler
+import androidx.core.os.postDelayed
 import com.github.anastr.rxlab.objects.drawing.ObserverObject
 import com.github.anastr.rxlab.objects.drawing.TextOperation
+import com.github.anastr.rxlab.objects.emits.BallEmit
+import com.github.anastr.rxlab.objects.time.TimeObject
 import com.github.anastr.rxlab.view.Action
 import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_operation.*
@@ -24,16 +27,21 @@ class IntervalActivity: OperationActivity() {
         val observerObject = ObserverObject("Observer")
         surfaceView.addDrawingObject(observerObject)
 
-        Observable.interval(2000, TimeUnit.MILLISECONDS)
-            .subscribe {
-                val thread = Thread.currentThread().name
-                surfaceView.action( Action(0) {
-                    val emit = BallEmit("${(it + 1) * 2} sec")
-                    emit.checkThread(thread)
-                    addEmit(intervalOperation, emit)
-                    moveEmit(emit, intervalOperation, observerObject)
-                })
-            }
-            .disposeOnDestroy()
+        Handler().postDelayed(500) {
+            var timeObject = observerObject.startTime(TimeObject.Lock.AFTER)
+            Observable.interval(2000, TimeUnit.MILLISECONDS)
+                .subscribe {
+                    val thread = Thread.currentThread().name
+                    surfaceView.action( Action(0) {
+                        timeObject.lock()
+                        val emit = BallEmit("${(it + 1) * 2} sec")
+                        emit.checkThread(thread)
+                        addEmit(intervalOperation, emit)
+                        timeObject = observerObject.startTime(TimeObject.Lock.AFTER)
+                        moveEmit(emit, intervalOperation, observerObject)
+                    })
+                }
+                .disposeOnDestroy()
+        }
     }
 }
