b4f4d361259d0b496c09263371fe83bb99583d9b
Anas Altair <anastr244@gmail.com>
2020-04-15 17:37:31 +0300

    add zip activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/activities/Zip2Activity.kt
@@ -0,0 +1,82 @@
+package com.github.anastr.rxlab.activities
+
+import android.os.Bundle
+import com.github.anastr.rxlab.objects.drawing.FixedEmitsOperation
+import com.github.anastr.rxlab.objects.drawing.ObserverObject
+import com.github.anastr.rxlab.objects.emits.BallEmit
+import com.github.anastr.rxlab.objects.emits.MergedBallEmit
+import com.github.anastr.rxlab.util.ColorUtil
+import com.github.anastr.rxlab.view.Action
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.functions.BiFunction
+import kotlinx.android.synthetic.main.activity_operation.*
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Anas Altair on 4/14/2020.
+ */
+class Zip2Activity: OperationActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCode("Observable o1 = Observable.just(\"A\", \"B\", \"C\", \"D\");\n" +
+                "Observable o2 = Observable.just(1, 2, 3, 4, 5);\n" +
+                "Observable.zip(o1, o2, (l, n) -> l +\"-\" + n)\n" +
+                "        .subscribe();")
+
+        val a = BallEmit("A")
+        val b = BallEmit("B")
+        val c = BallEmit("C")
+        val d = BallEmit("D")
+
+        val e1 = BallEmit("1", ColorUtil.blue)
+        val e2 = BallEmit("2", ColorUtil.blue)
+        val e3 = BallEmit("3", ColorUtil.blue)
+        val e4 = BallEmit("4", ColorUtil.blue)
+        val e5 = BallEmit("5", ColorUtil.blue)
+
+        val justLettersOperation = FixedEmitsOperation("just", listOf(a, b, c, d))
+        surfaceView.addDrawingObject(justLettersOperation)
+        val justNumbersOperation = FixedEmitsOperation("just", listOf(e1, e2, e3, e4, e5))
+        surfaceView.addDrawingObject(justNumbersOperation)
+        val zipOperation = FixedEmitsOperation("zip", ArrayList())
+        surfaceView.addDrawingObject(zipOperation)
+        val observerObject = ObserverObject("Observer")
+        surfaceView.addDrawingObject(observerObject)
+
+        val actions = ArrayList<Action>()
+
+        val observableLetters = Observable.just(a, b, c, d)
+            .doOnNext {
+            }
+        val observableNumbers = Observable.just(e1, e2, e3, e4, e5)
+            .doOnNext {
+            }
+
+        Observable.zip(observableLetters, observableNumbers
+            , BiFunction<BallEmit, BallEmit, MergedBallEmit> { emit1, emit2 ->
+                val mergedBallEmit = MergedBallEmit(emit2.position, emit1, emit2)
+                val thread = Thread.currentThread().name
+                actions.add(Action(0) { moveEmit(emit1, justLettersOperation, zipOperation) })
+                actions.add(Action(1000) { moveEmit(emit2, justNumbersOperation, zipOperation) })
+                actions.add(Action(500) {
+                    mergedBallEmit.checkThread(thread)
+                    doOnRenderThread {
+                        zipOperation.removeEmit(emit1)
+                        zipOperation.removeEmit(emit2)
+                    }
+                    addEmit(zipOperation, mergedBallEmit)
+                    moveEmit(mergedBallEmit, zipOperation, observerObject)
+                })
+                return@BiFunction mergedBallEmit
+            })
+            .delay(1000, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({}, errorHandler, {
+                actions.add(Action(0) { doOnRenderThread { observerObject.complete() } })
+                surfaceView.actions(actions)
+            })
+            .disposeOnDestroy()
+    }
+}
