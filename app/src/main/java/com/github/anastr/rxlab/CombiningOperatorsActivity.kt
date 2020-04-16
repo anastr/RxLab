05355f778a9e254ba5593c5dd3e8d8d4f568ba8c
Anas Altair <anastr244@gmail.com>
2020-04-16 22:23:03 +0300

    add combining and comparing list activity.
    Signed-off-by: Anas Altair <anastr244@gmail.com>

---
+++ app/src/main/java/com/github/anastr/rxlab/CombiningOperatorsActivity.kt
@@ -0,0 +1,41 @@
+package com.github.anastr.rxlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.anastr.rxlab.activities.MergeActivity
+import com.github.anastr.rxlab.activities.Zip2Activity
+import com.github.anastr.rxlab.activities.Zip3Activity
+import com.github.anastr.rxlab.adapter.MyAdapter
+import com.github.anastr.rxlab.adapter.OperationData
+import kotlinx.android.synthetic.main.content_list.*
+
+/**
+ * Created by Anas Altair on 4/16/2020.
+ */
+class CombiningOperatorsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        setContentView(R.layout.activity_list)
+        title = intent.getStringExtra("title")
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        val operations = listOf(
+            OperationData("merge", MergeActivity::class.java),
+            OperationData("zip 2 observables", Zip2Activity::class.java),
+            OperationData("zip 3 observables", Zip3Activity::class.java)
+        )
+
+        recyclerView.adapter = MyAdapter(this, operations)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
